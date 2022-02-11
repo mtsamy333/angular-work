fe2caf12e18dda6a659415fb10f4b62e9b5ae015
mtsamy333 <mtsamy333@gmail.com>
2022-02-11 12:43:01 +0530

    new

--- aagnia_project/src/app/app-routing.module.ts
+++ aagnia_project/src/app/app-routing.module.ts
@@ -1,30 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AccountsComponent } from './accounts/accounts.component';
 import { AddNewLeadComponent } from './add-new-lead/add-new-lead.component';
 import { AllLeadsComponent } from './all-leads/all-leads.component';
-import { ContactsComponent } from './contacts/contacts.component';
 import { EditLeadAagComponent } from './edit-lead-aag/edit-lead-aag.component';
-import { LeadsComponent } from './leads/leads.component';
-import { TasksComponent } from './tasks/tasks.component';
 
 const routes: Routes = [
-  {
-    path: "leads",
-    component: LeadsComponent
-  },
-  {
-    path: "contacts",
-    component: ContactsComponent
-  },
-  {
-    path: "accounts",
-    component: AccountsComponent
-  },
-  {
-    path: "tasks",
-    component: TasksComponent
-  },
   {
     path: "all-leads",
     component: AllLeadsComponent
--- aagnia_project/src/app/app.module.ts
+++ aagnia_project/src/app/app.module.ts
@@ -3,10 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LeadsComponent } from './leads/leads.component';
-import { AccountsComponent } from './accounts/accounts.component';
-import { ContactsComponent } from './contacts/contacts.component';
-import { TasksComponent } from './tasks/tasks.component';
 import { AllLeadsComponent } from './all-leads/all-leads.component';
 import { AddNewLeadComponent } from './add-new-lead/add-new-lead.component';
 import { EditLeadAagComponent } from './edit-lead-aag/edit-lead-aag.component';
@@ -14,10 +10,6 @@ import { EditLeadAagComponent } from './edit-lead-aag/edit-lead-aag.component';
 @NgModule({
   declarations: [
     AppComponent,
-    LeadsComponent,
-    AccountsComponent,
-    ContactsComponent,
-    TasksComponent,
     AllLeadsComponent,
     AddNewLeadComponent,
     EditLeadAagComponent
